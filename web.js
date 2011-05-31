ceac0a7b50e44d362858f95d30476198900e7d0c
Christian Sanz <chrissanz@gmail.com>
2011-05-31 12:30:02 -0700

    adding more goodies

--- web.js
+++ web.js
@@ -1,9 +1,43 @@
-var express = require('express');
+var express = require('express')
+  , stylus        = require('stylus')
+  , nib           = require('nib');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World From HEROKU BITCH! for more go here: http://devcenter.heroku.com/articles/node-js');
+//  Configure general expressjs
+ 
+app.configure(function(){
+ 
+ this
+   .use(express.cookieParser())
+   .use(express.bodyParser())
+   .use(express.errorHandler({dumpException: true, showStatck: true}))
+   .use(express.session({ secret: 'supersecretnijakey'}))
+});
+
+//  Configure template engine
+
+app.configure(function(){
+ this
+   .set('views', __dirname + '/views')
+   .set('view engine', 'jade')
+   .use(stylus.middleware({
+     src: __dirname + '/public',
+     compile: compile
+     }))
+   .use(express.static(__dirname + '/public'))
+});
+
+//  Small compile helper
+
+function compile(str, path){
+  return stylus(str)
+    .set('filename', path)
+    .include(nib.path)
+}
+
+app.get('/', function(req, res) {
+  res.render('home');
 });
 
 var port = process.env.PORT || 3000;
